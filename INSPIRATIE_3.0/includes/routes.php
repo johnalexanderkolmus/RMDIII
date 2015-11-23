b09bd6b6fa28813cd4aa628c1a24e53f4e7ea926
johnalexanderkolmus <john-alexander.kolmus@student.howest.be>
2015-11-23 16:49:37 +0100

    removed INSPIRATIE_3.0

--- INSPIRATIE_3.0/includes/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-$routes = array(
-    'overview' => array('controller' => 'Products', 'action' => 'overview'),
-    'register' => array('controller' => 'Products', 'action' => 'register'),
-    'app' => array('controller' => 'Products', 'action' => 'app')
-);
